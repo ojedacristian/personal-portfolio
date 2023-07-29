89a67603b23e1f18039144f4b43575c387bf8ff0
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2023-07-29 01:52:02 -0300

    About Page works in sm breakpoint

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,11 +5,11 @@ const AboutPage: NextPage = () => {
   return (
     <Layout dark>
       <section
-      className='container flex h-screen flex-col justify-around pt-20'>
-        <h1 className='text-center font-montserrat text-6xl font-bold uppercase tracking-[0.2em] md:text-7xl'>
+      className='container flex min-h-screen flex-col items-center justify-around pt-20'>
+        <h1 className='text-center font-montserrat text-6xl font-bold uppercase tracking-widest md:text-7xl md:tracking-[0.2em]'>
           <span className='text-brand-orange'>Creative </span>Studio
         </h1>
-        <div className='mx-auto w-3/4 md:w-2/3 xl:w-1/2'>
+        <div className='w-full px-4 md:w-3/4 xl:w-1/2'>
           <span className='mb-14 block h-1 w-64 bg-brand-lightGray md:w-96' />
           <p className='font-lato text-sm leading-7 text-[#999]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
         </div>
