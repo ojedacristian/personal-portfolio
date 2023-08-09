06fb36752ec5772e0ab27d8e28535d82f8c4dd64
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2023-08-08 21:14:24 -0300

    Add animations in About Page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,34 +1,55 @@
 import { Layout } from '@/components/Layout'
 import { type NextPage } from 'next'
+import { motion } from 'framer-motion'
+import { customVariant, customVariantItem } from '@/components/animations'
 
 const AboutPage: NextPage = () => {
   return (
     <Layout dark>
       <div className='mx-auto flex h-full px-8'
       >
-        <section
+        <motion.section
+         variants={customVariant}
+         initial='hidden'
+         animate='show'
           className='container flex min-h-screen flex-col items-center justify-around'>
-          <h1 className='text-center font-montserrat text-5xl font-bold uppercase tracking-widest md:text-6xl md:tracking-[0.2em] lg:text-7xl '>
+          <motion.h1 variants={customVariantItem} className='text-center font-montserrat text-5xl font-bold uppercase tracking-widest md:text-6xl md:tracking-[0.2em] lg:text-7xl '>
             <span className='text-brand-orange'>Multimedia </span>Developer
-          </h1>
-          <h2 className='w-5/6 px-2 text-center text-xl text-brand-darkGray [&>span]:text-brand-orange'>
+          </motion.h1>
+          <motion.h2 variants={customVariantItem} custom={1} className='w-5/6 px-2 text-center text-xl text-brand-darkGray [&>span]:text-brand-orange'>
             Mi nombre es <span>Cristian Ojeda</span> y soy un <span>profesional independiente</span> focalizado
             en la construcción de <span>experiencias multimedia</span>.
-          </h2>
+          </motion.h2>
           <div className='w-full px-4 lg:w-3/4 2xl:w-1/2'>
-            <span className='mb-4 block h-1 w-64 bg-brand-lightGray md:w-96' />
-            <p className='font-lato text-sm leading-7 text-[#999]'>
+            <motion.span
+            initial={{ scaleX: 0 }}
+            animate={{
+              scaleX: 1,
+              transition: {
+                ease: 'easeOut',
+                duration: 1.4,
+                delay: 0.6
+              }
+            }}
+            className='mb-4 block h-1 w-64 origin-left bg-brand-lightGray md:w-96' />
+            <motion.p variants={customVariantItem} custom={2} className='font-lato text-sm leading-7 text-[#999]'>
               Diseño y trabajo en el desarrollo de productos digitales y soluciones de comunicación gracias a un perfil multidisciplinario. Soy Licenciado en Tecnología Multimedia, lo que me llevo a gestionar proyectos en campos diversos como el diseño gráfico, web, interactivo, programación, análisis de datos y marketing digital.
-            </p>
+            </motion.p>
           </div>
-        </section>
+        </motion.section>
         {/* BACKGROUND */}
-        <div className='absolute left-0 top-36 -z-50 hidden overflow-hidden font-montserrat text-[500px] font-bold leading-none text-brand-lightGray opacity-30 sm:block sm:text-[600px] md:-left-0 md:-top-0 md:text-[800px]'>
+        <motion.div
+         initial={{ opacity: 0 }}
+         animate={{ opacity: 0.2 }}
+        className='absolute left-0 top-36 -z-50 hidden overflow-hidden font-montserrat text-[500px] font-bold leading-none text-brand-lightGray opacity-30 sm:block sm:text-[600px] md:-left-0 md:-top-0 md:text-[800px]'>
         S
-      </div>
-        <div className='absolute right-0 top-1/2 -z-50 overflow-hidden font-montserrat text-[200px] font-bold  leading-none tracking-widest text-brand-lightGray opacity-20'>
+      </motion.div>
+        <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.3, transition: { duration: 1, delay: 0.5 } }}
+        className='absolute right-0 top-1/2 -z-50 overflow-hidden font-montserrat text-[200px] font-bold  leading-none tracking-widest text-brand-lightGray opacity-20'>
           Dev
-        </div>
+        </motion.div>
       </div>
     </Layout>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import { ArrowRight } from '../components/icons'
 import { Layout } from '@/components/Layout'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import { customVariant, customVariantItem } from '@/components/animations'
 
 const HomePage: NextPage = () => {
   return (
     <Layout bg="bg-[url('/homebg2.jpg')] bg-cover bg-no-repeat h-screen overflow-hidden" >
       {/* BACKGROUND */}
       <div className='mx-auto flex h-full px-8'>
-        <div className='absolute left-2/3 top-1/2 z-10 flex h-full -translate-x-1/2 -translate-y-1/2 items-center overflow-hidden text-[400px] font-bold leading-[0] md:text-[600px]' id='mask'>
+        <div className='absolute left-3/4 top-1/2 z-10 flex h-full -translate-x-1/2 -translate-y-1/2 items-center overflow-hidden text-[400px] font-bold leading-[0] md:text-[600px]' id='mask'>
           <motion.h1
             initial={{
               opacity: 0, x: -250
@@ -18,39 +19,31 @@ const HomePage: NextPage = () => {
             }}
             className='font-montserrat'>js</motion.h1>
         </div>
-        <div className='absolute left-2/3 top-1/2 z-20 flex h-full -translate-x-1/2 -translate-y-1/2 items-center overflow-hidden text-[400px] font-bold leading-[0] text-brand-orange opacity-50 md:text-[600px]'>
+        <div className='absolute left-3/4 top-1/2 z-20 flex h-full -translate-x-1/2 -translate-y-1/2 items-center overflow-hidden text-[400px] font-bold leading-[0] text-brand-orange opacity-50 md:text-[600px]'>
           <motion.h1
             initial={{
-              opacity: 0
+              opacity: 0,
+              scale: 1
             }} animate={{ opacity: 1, transition: { duration: 1, delay: 1.5 } }}
             className='font-montserrat'>js</motion.h1>
         </div>
         {/* BACKGROUND END */}
-        <section
+        <motion.section
+          variants={customVariant}
+          initial='hidden'
+          animate='show'
           className='container relative z-30 flex items-center justify-center'>
           <div className='md:mr-40 lg:mr-60'>
             <motion.h1
-              initial={{ y: 20, opacity: 0 }}
-              animate={{
-                y: 0,
-                opacity: 1,
-                transition: { duration: 0.8 }
-              }}
+              variants={customVariantItem}
               className='font-montserrat text-4xl font-bold uppercase tracking-widest md:text-6xl md:tracking-widest'>
               Cristian Ojeda
             </motion.h1>
             <motion.h2
-              initial={{ y: 20, opacity: 0 }}
-              animate={{
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 0.3,
-                  duration: 0.8
-                }
-              }}
+              variants={customVariantItem}
+              custom={1}
               className='my-4 font-lato text-xl font-bold uppercase tracking-[0.15em] text-brand-lightGray md:text-brand-gray'>
-              Frontend Developer / Multimedia Designer
+              Frontend Developer / <span className='text-white'>Multimedia Designer</span>
             </motion.h2>
             <motion.span
               initial={{ scaleX: 0 }}
@@ -63,15 +56,8 @@ const HomePage: NextPage = () => {
               }}
               className='mb-7 mt-10 block h-[3px] w-2/5 origin-left bg-brand-orange' />
             <motion.div
-              initial={{ y: 20, opacity: 0 }}
-              animate={{
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 0.6,
-                  duration: 0.8
-                }
-              }}
+              variants={customVariantItem}
+              custom={2}
               className='flex items-center'>
               <Link href='/about' className='flex items-center'>
                 <p className='mr-2 font-montserrat text-sm font-bold uppercase'>
@@ -83,7 +69,7 @@ const HomePage: NextPage = () => {
               </Link>
             </motion.div>
           </div>
-        </section>
+        </motion.section>
       </div>
 
     </Layout>
